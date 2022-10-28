d08bb2303c40891245dcc8cbb4e90852e35a25a3
afederl <anton.federl@pfeiffercontrol.de>
2022-10-28 07:50:13 +0200

    Restructured projects.

---
+++ src/main/java/net/catena_x/btp/rul/oem/backend/rul_service/OemRulBackendServiceApplication.java
@@ -0,0 +1,20 @@
+package net.catena_x.btp.rul.oem.backend.rul_service;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@ComponentScan
+public class OemRulBackendServiceApplication {
+	public static void main(String[] args) {
+		new SpringApplicationBuilder()
+		.sources(OemRulBackendServiceApplication.class)
+		.profiles("rulbackendservice")
+		.run(args);
+	}
+}
