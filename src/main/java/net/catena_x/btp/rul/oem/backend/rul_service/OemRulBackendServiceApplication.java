604bce34e3818263ca1f8c4a21deecfbd766f17b
afederl <anton.federl@pfeiffercontrol.de>
2022-11-14 10:26:04 +0100

    Updated submodule (applied lost changes).

--- src/main/java/net/catena_x/btp/rul/oem/backend/rul_service/OemRulBackendServiceApplication.java
+++ src/main/java/net/catena_x/btp/rul/oem/backend/rul_service/OemRulBackendServiceApplication.java
@@ -3,12 +3,15 @@ package net.catena_x.btp.rul.oem.backend.rul_service;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class})
 @ComponentScan
 public class OemRulBackendServiceApplication {
 	public static void main(String[] args) {
